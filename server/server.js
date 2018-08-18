7d59a5d566e147aa49ff59ef3a7e066b451dc57a
Vinay Srivastava <vinay.srivastava@gmail.com>
2018-08-18 12:06:24 +0530

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
+
+const port = process.env.port || 3000;
+const app = express();
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
