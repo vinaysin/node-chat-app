75ad27359ca8b0f42e67385fa631c8f0fc1962ac
Vinay Srivastava <vinay.srivastava@gmail.com>
2018-08-18 12:28:33 +0530

    Updated server and package

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const publicPath = path.join(__dirname, '../public');
 
 const port = process.env.port || 3000;
-const app = express();
+var app = express();
 app.use(express.static(publicPath));
 
 app.listen(port, () => {
