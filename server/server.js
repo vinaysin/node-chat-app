1d0b1aac840d2e138ea14ef18cdfa3e9b3ca02f7
Vinay Srivastava <vinay.srivastava@gmail.com>
2018-08-18 12:47:33 +0530

    Fixed Port

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const publicPath = path.join(__dirname, '../public');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(publicPath));
 
